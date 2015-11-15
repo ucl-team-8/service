a8f58eee4b62fe5b4525cfc9e568828e8dc6b321
HappiApi <hasthanasombat.a@gmail.com>
2015-11-15 15:16:34 +0000

    Add Postgres database and SQL-alchemy dependencies

--- config.py
+++ config.py
@@ -1,9 +1,11 @@
+import os 
+
 class Config(object):
     DEBUG = False
     TESTING = False
     CSRF_ENABLED = True
     SECRET_KEY = "Don't Look!"
-
+    SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
 
 class ProductionConfig(Config):
     DEBUG = False
