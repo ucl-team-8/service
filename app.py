ba4c131c9ce1f2d46e0186ca1a5608e31c1365ba
HappiApi <hasthanasombat.a@gmail.com>
2015-11-15 15:02:49 +0000

    Initial Flask Setup

---
+++ app.py
@@ -0,0 +1,12 @@
+from flask import Flask
+import os
+
+app = Flask(__name__)
+app.config.from_object(os.environ['APP_SETTINGS'])
+
+@app.route("/")
+def hello():
+    return "Hello World!"
+
+if __name__ == "__main__":
+    app.run()
