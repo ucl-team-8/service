d42670db48e0a88c4f5c1c7a4acc838d06e70f98
Axel Goetz <axel.goetz@hotmail.com>
2015-11-23 16:30:58 +0000

    Added percentage to import

--- import/import.py
+++ import/import.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask.ext.sqlalchemy import SQLAlchemy
 import xml.etree.ElementTree as ET
 import datetime
+import sys
 import csv
 import os
 
@@ -15,10 +16,33 @@ from models import *
 
 def open_file(filename):
     try:
-        f = open(filename, 'rb')
+        f = open(os.path.dirname(os.path.realpath(__file__)) + '/' + filename, 'rb')
         return f
     except IOError:
         return None
+
+def file_length(file):
+    for i, l in enumerate(file):
+            pass
+    return i + 1
+
+def calculate_lengths(files):
+    open_files(files)
+    lengths = {'total_length': 0.0}
+    for key, file in files.items():
+        lengths[key] = file_length(file)
+        lengths['total_length'] += lengths[key]
+
+    lengths['finished'] = 0.0
+    close_files(files)
+    return lengths
+
+def update_progress(lengths):
+    lengths['finished'] += 1
+    progress = int((lengths['finished']/lengths['total_length'])*100)
+    percentage = (progress/5)
+    sys.stdout.write('\r[{0}{1}] {2}%'.format('#'*percentage, ' '*(20 - percentage),progress))
+    sys.stdout.flush()
 
 def set_date(time_string, separator):
     if time_string == '':
@@ -34,10 +58,11 @@ def delete_data():
     db.session.query(GPS).delete()
     db.session.query(UnitToGPSMapping).delete()
 
-def store_schedule(file):
+def store_schedule(file, lengths):
     try:
         reader = csv.DictReader(file)
         for row in reader:
+            update_progress(lengths)
             if len(row['Unit']) > 4: #Some columns contain weird unit values and no cif_uid
                 continue
 
@@ -48,10 +73,11 @@ def store_schedule(file):
     except KeyError:
         print "csv file is not in correct format"
 
-def store_trust(file):
+def store_trust(file, lengths):
     try:
         reader = csv.DictReader(file)
         for row in reader:
+            update_progress(lengths)
             arrival_report = set_date(row['Act Arrival Report'], '.')
             departure_report = set_date(row['Act Dep Report'], '.')
             origin_depart_time = set_date(row['Origin depart time'], '.')
@@ -64,10 +90,11 @@ def store_trust(file):
     except KeyError:
         print "csv file is not in correct format"
 
-def store_unit_to_gps(file):
+def store_unit_to_gps(file, lengths):
     try:
         reader = csv.DictReader(file)
         for row in reader:
+            update_progress(lengths)
             unit_to_gps = UnitToGPSMapping(row['Unit'], row['GPS car id'])
 
             db.session.add(unit_to_gps)
@@ -93,40 +120,42 @@ gps_column_translation = {
     'tiploc': 'tiploc'
 }
 
-def store_gps(file):
+def store_gps(file, lengths):
     root = ET.parse(file).getroot()
     gps_events = [gps_event.attrib for gps_event in root]
     rows = [translate_columns(gps_column_translation, gps_event) for gps_event in gps_events]
     for row in rows:
+        update_progress(lengths)
         row['event_time'] = datetime.datetime.strptime(row['event_time'], "%Y-%m-%dT%H:%M:%S")
         gps = GPS(**row)
         db.session.add(gps)
         db.session.commit()
 
-def import_file(filename, table_name):
-    f = open_file(filename)
-    if f == None:
-        print "File does not exist"
-        return
+def open_files(files):
+    files['schedule'] = open_file('schedule.csv')
+    files['unit_to_gps'] = open_file('unit_to_gps.csv')
+    files['trust'] = open_file('trust.csv')
+    files['gpsData'] = open_file('gpsData.xml')
 
-    if table_name == "schedule":
-        store_schedule(f)
-    elif table_name == "trust":
-        store_trust(f)
-    elif table_name == "unit_to_gps":
-        store_unit_to_gps(f)
-    elif table_name == "gps":
-        store_gps(f)
-    else:
-        print "Not a valid table name"
+def close_files(files):
+    for key, file in files.items():
+        file.close()
 
-    f.close()
+
 
 def main():
+    files = dict()
+    lengths = calculate_lengths(files)
+    open_files(files)
     delete_data()
-    import_file('schedule.csv', 'schedule')
-    import_file('unit_to_gps.csv', 'unit_to_gps')
-    import_file('trust.csv', 'trust')
-    import_file('gpsData.xml', 'gps')
+    store_schedule(files['schedule'], lengths)
+    store_unit_to_gps(files['unit_to_gps'], lengths)
+    store_trust(files['trust'], lengths)
+    store_gps(files['gpsData'], lengths)
+
+    close_files(files)
+    lengths['finished'] = lengths['total_length']
+    update_progress(lengths)
+    sys.stdout.write("\nSuccessfully added all files to db\n")
 
 main()
