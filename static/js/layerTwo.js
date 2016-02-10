7c35f4fc3b9288e4d97de32ee9ca8adc340dbfd5
HappiApi <hasthanasombat.a@gmail.com>
2016-02-10 09:01:34 +0000

    Sort data as d3.nest

--- static/js/layerTwo.js
+++ static/js/layerTwo.js
@@ -31,14 +31,28 @@ loadData().then(function(data) {
 })
 
 function processData(data){
-	_.forEach(data.trustData, function(d) {
+	_.forEach(data.trustData, function(d, i, c) {
 		d.event_time = new Date(d.event_time);
 		d.origin_departure = new Date(d.origin_departure);
 		d.tiploc = d.tiploc.trim();
 	})
+
+	data.trustData = d3.nest()
+	    .key(d => d.headcode)
+	    .sortValues((a, b) => d3.ascending(a.seq, b.seq))
+	    .key(d => d.gps_car_id).sortKeys(d3.ascending)
+	    .entries(data.trustData);
+
 	_.forEach(data.gpsData, function(d) {
 		d.event_time = new Date(d.event_time);
 	})
+
+	data.gpsData = d3.nest()
+	    .key(d => d.gps_car_id)
+	    .sortKeys(d3.ascending)
+	    .sortValues((a,b) => d3.ascending(a.event_time, b.event_time))
+	    .entries(data.gpsData);
+
 	_.forEach(data.scheduleData, function(d) {
 		d.origin_departure = new Date(d.origin_departure);
 	})
