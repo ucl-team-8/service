cafc7cc97a8f4878e8ddbc3db01fa2c829d48c87
Axel Goetz <axel.goetz@hotmail.com>
2016-02-18 13:52:47 +0000

    Code that adds gps_reports to the segments

---
+++ algorithm/filter_gps.py
@@ -0,0 +1,78 @@
+# File that is reponsible for adding gps_reports
+# to the segments
+
+# TODO: Backtracking to see if it can join
+# segments
+# TODO: In checkNonMatchingTrust should it also check
+# trust reports that have a match to see if this is a potential
+# closer match?
+# TODO: Should checkNonMatchingTrust choose the closest trust?
+
+from flask import Flask
+from flask.ext.sqlalchemy import SQLAlchemy
+import geo_distance
+import datetime
+import globals
+import time
+import os
+
+
+# Do backtracking across the segments with the same
+# gps_car_id
+
+
+# Finds the segment with the reports
+# that happened near the gps_report
+def findClosestSegment(segments, gps_report):
+    closest = {'segment': None, 'time_diff': datetime.timedelta(1000000)}
+    for segment in segments:
+        if segment.gps_car_id == gps_report['gps_car_id']:
+            time_diff = abs(segment.matching[-1]['gps']['event_time'] -
+                            gps_report['event_time'])
+            if time_diff < closest['time_diff']:
+                closest['segment'] = segment
+                closest['time_diff'] = time_diff
+    return closest['segment']
+
+
+# Checks if this gps report is a potential
+# match with a trust report in the segment
+def checkNonMatchingTrust(segment, gps_report):
+    for match in segment.matching:
+        if (match['trust'] is not None) and (match['gps'] is None):
+            time_error = abs(
+                match['trust']['event_time'] - gps_report['event_time'])
+            dist_err = geo_distance.calculateDistance(
+                match['trust']['tiploc'], gps_report['tiploc']
+            )
+            if dist_err < globals.tolerance['distance']\
+                    and time_err < globals.tolerance['time']:
+                match['gps'] = gps_report
+                match['dist_err'] = dist_err
+                match['time_err'] = time_err
+                return True
+    return False
+
+
+# This function adds the gps report to a segment
+def addGPS(gps_report):
+    potential_segments = []
+    globals.lock.acquire()
+    for segment in globals.segments:
+        if segment.gps_car_id == gps_report['gps_car_id']:
+            potential_segments.append(segment)
+
+    segment = findClosestSegment(potential_segments, gps_report)
+    if segment is None:
+        segment = globals.Segment()
+        segment.gps(gps_report)
+        globals.segments.append(segment)
+
+    elif not checkNonMatchingTrust(segment, gps_report):
+        segment.matching.append({
+            'gps': gps_report,
+            'trust': None,
+            'dist_err': None,
+            'time_error': None
+        })
+    globals.lock.release()
--- algorithm/simrealtime.py
+++ algorithm/simrealtime.py
@@ -6,14 +6,18 @@
 
 from flask import Flask
 from flask.ext.sqlalchemy import SQLAlchemy
+import concurrent.futures
+import filter_gps
 import threading
 import datetime
+import globals
 import time
 import os
 
 parentdir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-os.sys.path.insert(0,parentdir)
+os.sys.path.insert(0, parentdir)
 from models import *
+
 
 # This class fetches gps and trust events from the database
 # and simulates the realtime environment
@@ -33,11 +37,11 @@ class SimulateRealTime(threading.Thread):
         # If it is a newly declared records
         if records is None:
             records = db.session.query(model).\
-            order_by(model.event_time.asc()).limit(100)
+                order_by(model.event_time.asc()).limit(100)
         else:
             records = db.session.query(model).\
-            filter(model.event_time > records[-1]['event_time']).\
-            order_by(model.event_time.asc()).limit(100)
+                filter(model.event_time > records[-1]['event_time']).\
+                order_by(model.event_time.asc()).limit(100)
 
         # Extract individual records as dictionary
         return map(lambda x: x.as_dict(), records)
@@ -52,7 +56,8 @@ class SimulateRealTime(threading.Thread):
         elif current['name'] == 'gps':
             other_name = 'trust'
         else:
-            if self.records['gps'][0]['event_time'] < self.records['trust'][0]['event_time']:
+            if self.records['gps'][0]['event_time'] < self.\
+                    records['trust'][0]['event_time']:
                 current['name'] = 'gps'
                 current['record'] = self.records['gps'][0]
             else:
@@ -60,7 +65,8 @@ class SimulateRealTime(threading.Thread):
                 current['record'] = self.records['trust'][0]
             return
 
-        if self.records[current['name']][1]['event_time'] < self.records[other_name][0]['event_time']:
+        if self.records[current['name']][1]['event_time'] < self.\
+                records[other_name][0]['event_time']:
             next['name'] = current['name']
             next['record'] = self.records[current['name']][1]
         else:
@@ -72,35 +78,48 @@ class SimulateRealTime(threading.Thread):
     # my need the first element
     def checkEmptyRecords(self):
         if len(self.records['gps']) == 1:
-            self.records['gps'].extend(self.fetchEvents(GPS, self.records['gps']))
+            self.records['gps'].extend(
+                self.fetchEvents(GPS, self.records['gps'])
+            )
         elif len(self.records['trust']) == 1:
-            self.records['trust'].extend(self.fetchEvents(Trust, self.records['trust']))
+            self.records['trust'].extend(
+                self.fetchEvents(Trust, self.records['trust'])
+            )
 
     # Gets the next record and then sleeps the time between the next
     # record and the current record
     # finally deletes the current record and replaces it with next
-    def getNextRecordAndSleep(self, current, next):
+    def getNextRecordAndSleep(self, executor, current, next):
         self.closestRecordName(current, next)
-        time_to_sleep = (next['record']['event_time'] - \
-        current['record']['event_time']).total_seconds()
+        time_to_sleep = (
+            next['record']['event_time'] - current['record']['event_time']
+            ).total_seconds()
 
         time.sleep(time_to_sleep/self.speed)
 
-        print(next['record'])
+        executor.submit(self.performAction, next)
 
         del self.records[current['name']][0]
         current['name'] = next['name']
         current['record'] = next['record']
 
-    def run(self):
-        current = {'name': '', 'record': None}
-        next = {'name': '', 'record': None}
-        self.closestRecordName(current, next)
-        print(current['record'])
+    def performAction(self, object):
+        print object['record']
+        if object['name'] == 'gps':
+            filter_gps.addGPS(object['record'])
+        print globals.segments
 
-        while len(self.records['gps']) > 0 or len(self.records['trust']) > 0:
-            self.checkEmptyRecords()
-            self.getNextRecordAndSleep(current, next)
+    def run(self):
+        with concurrent.futures.ThreadPoolExecutor(max_workers=5) as executor:
+            current = {'name': '', 'record': None}
+            next = {'name': '', 'record': None}
+            self.closestRecordName(current, next)
+            executor.submit(self.performAction, current)
+
+            while len(self.records['gps']) > 0 or\
+                    len(self.records['trust']) > 0:
+                self.checkEmptyRecords()
+                self.getNextRecordAndSleep(executor, current, next)
 
 temp = SimulateRealTime(100)
 temp.start()
