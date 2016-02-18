024e1c7690403dc75ac8f6b0913150c1356b0084
Axel Goetz <axel.goetz@hotmail.com>
2016-02-18 20:27:49 +0000

    database queries

---
+++ algorithm/db_queries.py
@@ -0,0 +1,76 @@
+# File that contains function, which do querying
+
+from flask.ext.sqlalchemy import SQLAlchemy
+from sqlalchemy.sql import func, and_, or_
+from flask import Flask
+import os
+
+parentdir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+os.sys.path.insert(0, parentdir)
+
+from models import *
+
+
+# Function that check if a unit was supposed
+# to run a service with the headcode
+def isPlanned(unit, headcode):
+    result = db.session.query(Schedule).filter(and_(
+        Schedule.unit == unit, Schedule.headcode == headcode
+    ))
+    try:
+        temp = result[0].as_dict()
+        return True
+    except:
+        return False
+
+
+# Gets the unit from a gps_car_id by querying
+# the UnitToGPSMapping table
+def getUnitFromCarId(gps_car_id):
+    result = db.session.query(UnitToGPSMapping).filter(
+        UnitToGPSMapping.gps_car_id == gps_car_id)
+    try:
+        return result[0].as_dict()['unit']
+    except:
+        return ''
+
+
+# Gets the diagram_service row for a
+# particular unit
+def getDiagramServiceByUnit(unit):
+    result = db.session.query(DiagramService).filter(
+        DiagramService.unit == unit
+    )
+    try:
+        return result[0].as_dict()
+    except:
+        return None
+
+
+# gets the rows from diagram_stop for a particular
+# diagram service
+def getDiagramStopsByService(diagram_service):
+    result = db.session.query(DiagramStop).filter(
+        DiagramStop.diagram_service_id == diagram_service['id']
+    )
+    return map(lambda x: x.as_dict(), result)
+
+
+# Gets all of the diagram stops for a service
+def getDiagramStopsByUnit(unit):
+    diagram_service = getDiagramServiceByUnit(unit)
+    return getDiagramStopsByService(diagram_service)
+
+
+# Gets the cif_uid from the Schedule using unit and headcode
+def cif_uidFromUnitAndHeadcode(unit, headcode):
+    result = db.session.query(Schedule).filter(and_(
+        Schedule.unit == unit, Schedule.headcode == headcode
+    ))
+    try:
+        temp = result[0].as_dict()['cif_uid']
+        if temp is None:
+            return ''
+        return temp
+    except:
+        return ''
