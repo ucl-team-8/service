c1a891bc841fd8d8b5e559245ee6f5bbdeb1914a
Daniel Gavrilov <dgavrilov95@gmail.com>
2016-03-10 22:12:35 +0000

    Use segments data in map

--- static/js/map/index.js
+++ static/js/map/index.js
@@ -3,10 +3,10 @@ import d3 from "d3";
 import L from "leaflet";
 
 import RouteMap from "./route-map";
-import { getTrust, getGPS, getLocations } from "./data";
+import { getSegments, getLocations } from "./data";
 
 let transportLayer = new L.TileLayer("http://{s}.tile.thunderforest.com/transport/{z}/{x}/{y}.png")
-    .setOpacity(0.25);
+    .setOpacity(0.15);
 
 let map = new L.Map("map", {center: [53.5, -1.5], zoom: 7})
     .addLayer(transportLayer);
@@ -16,25 +16,41 @@ let routeMap = new RouteMap(map);
 window.locations = {};
 
 Promise.all([
-  getTrust(),
-  getGPS(),
+  getSegments(),
   getLocations()
-]).then(([trust, gps, locations]) => {
+]).then(([segments, locations]) => {
 
   window.locations = _.keyBy(locations, "tiploc");
 
   let services = d3.nest()
-      .key(d => d.headcode)
-      .sortValues((a, b) => d3.ascending(a.seq, b.seq))
-      .entries(trust);
+    .key(d => d.headcode)
+    .entries(segments);
 
-  let units = d3.nest()
-      .key(d => d.gps_car_id)
-      .sortKeys(d3.ascending)
-      .sortValues((a,b) => d3.ascending(a.event_time, b.event_time))
-      .entries(gps);
+  let serviceSegments = services[8].values;
 
-  routeMap.plotServices([services[30].values]);
-  routeMap.plotUnits([units[26].values])
+  let serviceStops = getServiceStopsFromSegments(serviceSegments);
+  let units = serviceSegments.map(getUnitStopsFromSegment);
+
+  routeMap.plotServices([serviceStops]);
+  routeMap.plotUnits(units);
 
 });
+
+function getServiceStopsFromSegments(segments) {
+  return _(segments)
+    .map(d => d.matching)
+    .flatten()
+    .map(d => d.trust)
+    .uniqWith(_.isEqual)
+    .compact()
+    .sortBy(d => d.event_time)
+    .value();
+}
+
+function getUnitStopsFromSegment(segment) {
+  return _(segment.matching)
+    .map(d => d.gps)
+    .compact()
+    .sortBy(d => d.event_time)
+    .value();
+}
