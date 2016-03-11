d0dc9ccab38a41434c87dc8ffa9a3414a58392a3
Daniel Gavrilov <dgavrilov95@gmail.com>
2016-03-11 14:09:54 +0000

    Plot and move through segments with arrow keys

--- static/js/map/index.js
+++ static/js/map/index.js
@@ -5,6 +5,13 @@ import L from "leaflet";
 import RouteMap from "./route-map";
 import { getSegments, getLocations } from "./data";
 
+/*
+
+IMPORTANT: All the code in this file is sort of hacked together and you probably
+shouldn't add functionality here, instead make a new module or something.
+
+*/
+
 let transportLayer = new L.TileLayer("http://{s}.tile.thunderforest.com/transport/{z}/{x}/{y}.png")
     .setOpacity(0.15);
 
@@ -22,21 +29,34 @@ Promise.all([
 
   window.locations = _.keyBy(locations, "tiploc");
 
-  let services = d3.nest()
-    .key(d => d.headcode)
-    .entries(segments);
+  let i = 0;
+  segments = _.sortBy(segments, "headcode");
 
-  let serviceSegments = services[8].values;
+  plotSegment(segments[i]);
 
-  let serviceStops = getServiceStopsFromSegments(serviceSegments);
-  let units = serviceSegments.map(getUnitStopsFromSegment);
+  // routeMap.time(new Date("Tue Mar 17 2015 11:38:00"));
 
-  routeMap.plotServices([serviceStops]);
-  routeMap.plotUnits(units);
+  window.routeMap = routeMap;
 
-  routeMap.time(new Date("Tue Mar 17 2015 11:38:00"));
+  window.addEventListener("keydown", function(event) {
+    if (event.keyCode == 37) {
+      plotSegment(segments[--i]);
+      event.preventDefault();
+    } else if (event.keyCode == 39) {
+      plotSegment(segments[++i]);
+      event.preventDefault();
+    }
+  });
 
 });
+
+function plotSegment(segment) {
+  console.log(`Plotting ${segment.headcode || "_"} ${segment.gps_car_id || "_"}`, segment);
+  let serviceStops = getServiceStopsFromSegment(segment);
+  let unitStops = getUnitStopsFromSegment(segment);
+  routeMap.plotServices([serviceStops]);
+  routeMap.plotUnits([unitStops]);
+}
 
 function getServiceStopsFromSegments(segments) {
   return _(segments)
@@ -44,6 +64,14 @@ function getServiceStopsFromSegments(segments) {
     .flatten()
     .map(d => d.trust)
     .uniqWith(_.isEqual)
+    .compact()
+    .sortBy(d => d.event_time)
+    .value();
+}
+
+function getServiceStopsFromSegment(segment) {
+  return _(segment.matching)
+    .map(d => d.trust)
     .compact()
     .sortBy(d => d.event_time)
     .value();
