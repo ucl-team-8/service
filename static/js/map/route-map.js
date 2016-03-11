d35cfd0ff0114ce5f2dfb9f4e71e8265c1238d0d
Daniel Gavrilov <dgavrilov95@gmail.com>
2016-03-11 00:25:15 +0000

    Add some comments to RouteMap

--- static/js/map/route-map.js
+++ static/js/map/route-map.js
@@ -4,6 +4,17 @@ import d3 from "d3";
 import { tiplocToPoint } from "./location";
 import Route from "./route";
 
+/*
+
+Provides methods for plotting TRUST & GPS reports on a Leaflet.js map.
+
+The Leaflet.js map should already exist and be passed as a parameter to the
+constructor.
+
+Much of the code is from https://bost.ocks.org/mike/leaflet/.
+
+*/
+
 export default class RouteMap {
 
   constructor(map) {
@@ -25,26 +36,52 @@ export default class RouteMap {
     this.redraw();
   }
 
+  /*
+  Returns all Route objects that are plotted on the map.
+  */
   allRoutes() {
     return this.services.concat(this.units);
   }
 
+  /*
+
+  Plots the given array of services on a map. A single service consists of an
+  array of stops, e.g.
+
+      var service = [
+        { tiploc: "KNGX", event_time: ... },
+        { tiploc: ..., event_time: ... }
+      ];
+
+  */
   plotServices(services) {
     this.services.forEach(d => d.destroy());
     this.services = services.map(d => new Route(this.map, this.servicesContainer, d, "trust"));
     this.redraw();
   }
 
+  /*
+  Plots the given array of units on a map. A single unit consists of an array of
+  stops, just like a service (see above).
+  */
   plotUnits(units) {
     this.units.forEach(d => d.destroy());
     this.units = units.map(d => new Route(this.map, this.unitsContainer, d, "gps"));
     this.redraw();
   }
 
+  /*
+  Given a Date object, it grays out all the stops that happened in the future.
+  Pass `null` to reset.
+  */
   time(t) {
     this.allRoutes().forEach(d => d.setTime(t));
   }
 
+  /*
+  Redraws the all routes. This method is called automatically in case of updated
+  services, or an action like zoom.
+  */
   redraw() {
 
     const allRoutes = this.allRoutes().map(d => d.data);
@@ -73,6 +110,11 @@ export default class RouteMap {
     this.units.forEach(d => d.redraw());
   }
 
+  /*
+  Removes all the plots, as well as container elements used for drawing.
+  After executing this, the instance is unusable and you need to create a new
+  one if you want to continue plotting.
+  */
   destroy() {
     this.destroyContents();
     this.map.off("viewreset", this.redraw);
