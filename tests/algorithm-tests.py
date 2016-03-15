468a3479116acf2e2fedd731642b34afdbfcb37c
HappiApi <hasthanasombat.a@gmail.com>
2016-03-15 11:19:06 +0000

    Fix distance check to work across OS for CI

--- tests/algorithm-tests.py
+++ tests/algorithm-tests.py
@@ -7,6 +7,7 @@ os.sys.path.insert(0, parentdir + '/algorithm')
 import geo_distance
 import db_queries
 
+tolerance = 0.001
 coord1 = {'latitude': 52.2296756, 'longitude': 21.0122287}
 coord2 = {'latitude': 52.406374, 'longitude': 16.9251681}
 coord3 = {'latitude': 57.4774160525352, 'longitude': -4.46815579590345}
@@ -15,23 +16,23 @@ coord4 = {'latitude': 51.6909417951552, 'longitude': -3.40287716811208}
 
 class TestGeoDist(unittest.TestCase):
     def test_distance1(self):
-        self.assertEqual(
-            geo_distance.calculateDist(coord1, coord2), 278.54558935106695
+        self.assertLess(
+            abs(geo_distance.calculateDist(coord1, coord2) - 278.54558935106695), tolerance
         )
 
     def test_distance2(self):
-        self.assertEqual(
-            geo_distance.calculateDist(coord3, coord4), 647.259510348751
+        self.assertLess(
+            abs(geo_distance.calculateDist(coord3, coord4) - 647.259510348751), tolerance
         )
 
     def test_tiploc_long_lat(self):
         result = geo_distance.findLongLat('BEAULY')
-        self.assertEqual(result['latitude'], 57.4774160525352)
-        self.assertEqual(result['longitude'], -4.46815579590345)
+        self.assertLess(abs(result['latitude'] - 57.4774160525352), tolerance)
+        self.assertLess(abs(result['longitude'] - -4.46815579590345), tolerance)
 
     def test_calculate_distance(self):
         result = geo_distance.calculateDistance('BEAULY', 'ABCWM')
-        self.assertEqual(result, 647.259510348751)
+        self.assertLess(result - 647.259510348751, tolerance)
 
 
 class TestDBQueries(unittest.TestCase):
