7523c68227bb40d05d03554af368949ebea0f5ff
Daniel Gavrilov <dgavrilov95@gmail.com>
2016-04-10 22:27:53 +0100

    Add comments

--- algorithm2/env.py
+++ algorithm2/env.py
@@ -1,13 +1,19 @@
 from datetime import datetime
 
+# effectively replaces datetime.now() since we simulate the passing of time
 global now
 now = datetime.fromtimestamp(0) # initialise with epoch time
 
+# minutes events are stored in cache
+# this is so we can match a report that occurred some time ago, but the system
+# only received it now
 global retention_minutes
 retention_minutes = 30
 
+# maximum minutes difference for events to match at the same tiploc
 global within_minutes
 within_minutes = 5
 
+# minutes between running the service matching algorithm
 global run_delay
 run_delay = 30
