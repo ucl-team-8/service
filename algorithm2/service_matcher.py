016804960866a40261b3d432750a0e7ce59e0293
Daniel Gavrilov <dgavrilov95@gmail.com>
2016-04-11 15:37:19 +0100

    Use quicker bulk operations in service matcher

--- algorithm2/service_matcher.py
+++ algorithm2/service_matcher.py
@@ -1,4 +1,5 @@
 from app_db import db
+from db_queries import get_event_matchings, get_service_matching
 import env
 from models import EventMatching, ServiceMatching
 from utils import diff_seconds, median, variance
@@ -32,14 +33,30 @@ class ServiceMatcher:
         """Takes the primary keys of the service matchings (from the queue) that
            need to be updated, updates them and stores them in the database.
         """
+
         changed_matchings = self.queue.pop_changed_matchings()
+
+        insert, update = [], []
+
+        # TODO: can be more efficient if it gets existing primary keys with a
+        # single query, rather than querying for each one.
+
         for service, unit in changed_matchings:
-            service_matching = self.get_service_matching(service, unit)
-            db.session.merge(service_matching)
+
+            service_matching = self.get_service_matching_props(service, unit)
+            if get_service_matching(service, unit):
+                update.append(service_matching)
+            else:
+                insert.append(service_matching)
+
+        db.session.bulk_insert_mappings(ServiceMatching, insert)
+        db.session.bulk_update_mappings(ServiceMatching, update)
+
         db.session.commit()
+
         # TODO: socketio notify each changed
 
-    def get_service_matching(self, service, unit):
+    def get_service_matching_props(self, service, unit):
         """Returns an instance of ServiceMatching (a table row essentially) with
         all the fields calculated and populated.
         """
@@ -47,11 +64,7 @@ class ServiceMatcher:
         headcode, origin_location, origin_departure = service
         gps_car_id = unit
 
-        event_matchings = db.session.query(EventMatching).filter_by(
-            headcode=headcode,
-            origin_location=origin_location,
-            origin_departure=origin_departure,
-            gps_car_id=gps_car_id)
+        event_matchings = get_event_matchings(service, unit)
 
         trust_times = [m.trust_event_time for m in event_matchings]
         gps_times = [m.gps_event_time for m in event_matchings]
@@ -62,16 +75,14 @@ class ServiceMatcher:
 
         time_errors = [diff_seconds(a, b) / 60.0 for a, b in zip(trust_times, gps_times)]
 
-        service_matching = ServiceMatching(
-            headcode=headcode,
-            origin_location=origin_location,
-            origin_departure=origin_departure,
-            gps_car_id=gps_car_id,
-            total_matching=len(time_errors),
-            median_time_error=median(time_errors),
-            variance_time_error=variance(time_errors),
-            start=start,
-            end=end
-        )
-
-        return service_matching
+        return {
+            'headcode': headcode,
+            'origin_location': origin_location,
+            'origin_departure': origin_departure,
+            'gps_car_id': gps_car_id,
+            'total_matching': len(time_errors),
+            'median_time_error': median(time_errors),
+            'variance_time_error': variance(time_errors),
+            'start': start,
+            'end': end
+        }
