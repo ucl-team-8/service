db3341a7dceec14fc2afd08312052af41778b16f
Daniel Gavrilov <dgavrilov95@gmail.com>
2016-04-12 23:48:52 +0100

    Implement get_service_matchings_by_keys db query

--- algorithm2/db_queries.py
+++ algorithm2/db_queries.py
@@ -1,4 +1,6 @@
 from app_db import app
+import sqlalchemy.sql
+from sqlalchemy.sql import and_, or_
 from models import EventMatching, ServiceMatching
 
 from sqlalchemy import create_engine
@@ -25,10 +27,20 @@ def get_service_matching(service, unit):
 def get_service_matchings_for_unit(unit):
     return db_session.query(ServiceMatching).filter_by(gps_car_id=unit)
 
-# TODO: finish this
-# def get_service_maching_existing_keys(pkeys):
-#     db_session.query(
-#         ServiceMatching.headcode,
-#         ServiceMatching.origin_location,
-#         ServiceMatching.origin_departure
-#     ).filter()
+def get_service_matchings_by_keys(pkeys):
+    if not pkeys: return db_session.query(ServiceMatching).filter(sqlalchemy.sql.false())
+    pkey_predicates = map(construct_pkey_predicate, pkeys)
+    return db_session.query(
+        ServiceMatching.headcode,
+        ServiceMatching.origin_location,
+        ServiceMatching.origin_departure,
+        ServiceMatching.gps_car_id
+    ).filter(
+        or_(*pkey_predicates)
+    )
+
+def construct_pkey_predicate(pkey):
+    return and_(ServiceMatching.headcode==pkey[0],
+                ServiceMatching.origin_location==pkey[1],
+                ServiceMatching.origin_departure==pkey[2],
+                ServiceMatching.gps_car_id==pkey[3])
