5f519ee50f0287c3eda3d29f2d2aa39531862f1e
Daniel Gavrilov <dgavrilov95@gmail.com>
2016-04-12 17:21:45 +0100

    Store unit allocations in Allocations
    So that they are easier to access.

--- algorithm2/allocations.py
+++ algorithm2/allocations.py
@@ -3,29 +3,49 @@ from models import Schedule
 from db_queries import db_session
 
 class Allocations:
+    """Stores the genius allocations (and provides useful functions for them).
+    """
 
     def __init__(self):
-        self.allocations = self.__get_allocations()
-
-    def get_allocations(self):
-        return copy.deepcopy(self.allocations)
+        self.service_allocations, self.unit_allocations = self.__get_allocations()
 
     def get_units_for_service(self, service):
-        if service not in self.allocations:
+        """Returns a set of the units allocated to a service. It expects the
+        service as a tuple of (headcode, origin_location, origin_departure)
+        """
+        if service not in self.service_allocations:
             return set()
         else:
-            return copy.deepcopy(self.allocations[service])
+            return copy.deepcopy(self.service_allocations[service])
+
+    def get_services_for_unit(self, unit):
+        """Returns a set of services allocated to a unit. It expects a string of
+        the gps_car_id.
+        """
+        if unit not in self.unit_allocations:
+            return set()
+        else:
+            return copy.deepcopy(self.unit_allocations[unit])
 
     def __get_allocations(self):
+        """Retrieves the allocations from the database.
+        """
 
         query = db_session.query(Schedule).all()
-        allocations = dict()
+        service_allocations = dict()
+        unit_allocations = dict()
 
         for alloc in query:
+
             service = (alloc.headcode, alloc.origin_location, alloc.origin_departure)
             unit = alloc.unit
-            if service not in allocations:
-                allocations[service] = set()
-            allocations[service].add(unit)
 
-        return allocations
+            if service not in service_allocations:
+                service_allocations[service] = set()
+            service_allocations[service].add(unit)
+
+            if unit not in unit_allocations:
+                unit_allocations[unit] = set()
+            unit_allocations[unit].add(service)
+
+        return (service_allocations, unit_allocations)
