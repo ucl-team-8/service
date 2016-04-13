203668cbc31016caabe590229975e1c5bf4f177d
Daniel Gavrilov <dgavrilov95@gmail.com>
2016-04-14 00:04:23 +0100

    Concurrency: doing it right

--- algorithm2/dispatcher.py
+++ algorithm2/dispatcher.py
@@ -1,41 +1,48 @@
 import env
+import threading
 
 class Dispatcher:
 
     def __init__(self, socketio):
         self.socketio = socketio
         self.listeners = dict()
+        self.lock = threading.RLock()
 
     def subscribe(self, topic, sid):
         print("subscribing", sid, "to", topic)
-        if topic not in self.listeners:
-            self.listeners[topic] = set()
-        listeners = self.listeners[topic]
-        listeners.add(sid)
+        with self.lock:
+            if topic not in self.listeners:
+                self.listeners[topic] = set()
+            listeners = self.listeners[topic]
+            listeners.add(sid)
 
     def unsubscribe(self, topic, sid):
         print("unsubscribing", sid, "from", topic)
-        if topic not in self.listeners:
-            return
-        listeners = self.listeners[topic]
-        listeners.discard(sid)
+        with self.lock:
+            if topic not in self.listeners:
+                return
+            listeners = self.listeners[topic]
+            listeners.discard(sid)
 
     def unsubscribe_from_all(self, sid):
         print("unsubscribing", sid, "from all")
-        for topic in self.listeners:
-            self.listeners[topic].discard(sid)
+        with self.lock:
+            for topic in self.listeners:
+                self.listeners[topic].discard(sid)
 
     def has_listeners(self, topic):
-        return topic in self.listeners and \
-               len(self.listeners[topic]) > 0
+        with self.lock:
+            return topic in self.listeners and \
+                   len(self.listeners[topic]) > 0
 
     def dispatch(self, topic, *args):
-        if topic not in self.listeners:
-            return
-        print("dispatching to", topic)
-        for sid in self.listeners[topic]:
-            self.socketio.emit(topic, *args, room=sid)
-        # self.socketio.emit(topic, *args)
+        with self.lock:
+            if topic not in self.listeners:
+                return
+            print("dispatching to", topic)
+            for sid in self.listeners[topic]:
+                self.socketio.emit(topic, *args, room=sid)
+            # self.socketio.emit(topic, *args)
 
     def dispatch_service(self, service, segments):
         print('dispatching service', service)
