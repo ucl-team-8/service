a168b4ccc82a179cf4248604d71e851629b024c9
Daniel Gavrilov <dgavrilov95@gmail.com>
2016-04-14 12:23:00 +0100

    Add get trust/gps reports db queries

--- algorithm2/db_queries.py
+++ algorithm2/db_queries.py
@@ -1,7 +1,7 @@
 from app_db import app
 import sqlalchemy.sql
 from sqlalchemy.sql import and_, or_
-from models import EventMatching, ServiceMatching
+from models import Trust, GPS, EventMatching, ServiceMatching
 
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
@@ -10,6 +10,29 @@ engine = create_engine(app.config['SQLALCHEMY_DATABASE_URI'])
 db_session = scoped_session(sessionmaker(bind=engine,
                                          autoflush=True,
                                          autocommit=False))
+
+def get_trust_reports(service, start=None, end=None):
+    predicates = []
+    headcode, origin_location, origin_departure = service
+    if start: predicates.append(Trust.event_time >= start)
+    if end: predicates.append(Trust.event_time <= end)
+    trust_reports = db_session.query(Trust).filter_by(
+        headcode=headcode,
+        origin_location=origin_location,
+        origin_departure=origin_departure
+    ).filter(*predicates).all()
+    db_session.close()
+    return trust_reports
+
+def get_gps_reports(gps_car_id, start=None, end=None):
+    predicates = []
+    if start: predicates.append(GPS.event_time >= start)
+    if end: predicates.append(GPS.event_time <= end)
+    gps_reports = db_session.query(GPS).filter_by(
+        gps_car_id=gps_car_id
+    ).filter(*predicates).all()
+    db_session.close()
+    return gps_reports
 
 def get_event_matchings(service, unit):
     headcode, origin_location, origin_departure = service
